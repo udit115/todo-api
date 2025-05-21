28600e6e69b5f32555d93926500502ed7897b615
udit_115 <uditsingh9368@gmail.com>
2025-05-21 15:24:44 +0530

    update code

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const app = express();
-const port = 3000;
+const port = 5000 ;
 app.listen(port, () => {
     console.log("server is running at port 3000");
 
